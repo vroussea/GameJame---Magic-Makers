377a3572a3185f5d0a6fe29c5b61e48cc5e08793
vroussea <rousseau59>
2019-02-14 13:30:49 +0100

    14/02/2019 - character can now move - add beginning for screen scrolling - add entity super class for player and platforms - add platform class

--- game/classes/character.py
+++ game/classes/character.py
@@ -1,4 +1,36 @@
-class Character:
-    def __init__(self, name):
-        assert isinstance(name, str)
-        self.name = name
+import contextlib
+
+from game.classes.entity import Entity
+
+with contextlib.redirect_stdout(None):
+    import pygame
+from pygame import *
+
+
+class Character(Entity):
+    def __init__(self, screen):
+        super().__init__()
+        x, y = pygame.display.get_surface().get_size()
+        self.rect = pygame.draw.rect(screen, (0, 255, 0), pygame.Rect((x / 2) - 20, (y / 2) - 20, 40, 40))
+
+    def update(self):
+        pressed = pygame.key.get_pressed()
+        up = pressed[K_UP]
+        down = pressed[K_DOWN]
+        left = pressed[K_LEFT]
+        right = pressed[K_RIGHT]
+
+        if up:
+            self.rect.move_ip(0, -1)
+        if down:
+            self.rect.move_ip(0, 1)
+        if left:
+            self.rect.move_ip(-1, 0)
+        if right:
+            self.rect.move_ip(1, 0)
+
+    def scrolling(self):
+        self.rect.move_ip(-1, 0)
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, (0, 255, 0), self.rect)
--- game/main.py
+++ game/main.py
@@ -1,16 +1,25 @@
-#! /usr/bin/python
-
 import contextlib
 
 with contextlib.redirect_stdout(None):
-    import pygame
+    pass
 from pygame import *
 
+from game.classes.character import Character
+
+# Put the window at the center of the screen (for debug)
+
+import pygame, os
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
 
 def main():
-    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     pygame.init()
+    width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
+    screen = pygame.display.set_mode((width, height))
     timer = pygame.time.Clock()
+
+    player = Character(screen)
+    time_elapsed_since_last_action = 0
 
     while 1:
         for e in pygame.event.get():
@@ -19,8 +28,18 @@ def main():
             if e.type == KEYDOWN and e.key == K_ESCAPE:
                 return
 
+        player.update()
+
         screen.fill((0, 0, 0))
+
+        if time_elapsed_since_last_action > 1:
+            player.scrolling()
+            time_elapsed_since_last_action = 0
+
+        player.draw(screen)
+
         pygame.display.update()
+        time_elapsed_since_last_action += timer.tick()
         timer.tick(60)
 
 
